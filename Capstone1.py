3b71f34f84cf7912e71b85d253f74ef30712e894
aryansharma2779 <aryan.sharma_da17@gla.ac.in>
2019-07-20 20:31:26 +0530

    Python to git

---
+++ Capstone1.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# This Notebook will be mainly use for Capstone Projects
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
